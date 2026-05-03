c6f21a748b84c25c56afbc85afdccaf28ead5ab1
Kushal Ghimire <kushal.upr@gmail.com>
2026-05-03 12:49:02 +0545

    inputtype

--- Chapter 2/input.py
+++ Chapter 2/input.py
@@ -1,5 +1,10 @@
-a = input("Enter the number 1: ")
-b = input("Enter the number 2: ")
+# sum of the given input number
+a = int(input("Enter the number 1: "))
+b = int(input("Enter the number 2: "))
+
 
 print ("Number a: ",a)
 print ("Number b: ",b)
+
+print("sum is :", a+b)
+ 
