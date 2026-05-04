f41a61ae5d8263db21bc3ce0f060beffd48fbbd8
Kushal Ghimire <kushal.upr@gmail.com>
2026-05-04 21:36:25 +0545

    check the number is multipication by any number

---
+++ Chapter 2/check_num.py
@@ -0,0 +1,9 @@
+#check the number is multipication by 7 or not
+
+
+n =int(input("Enter the number"))
+if(n % 7 == 0):
+    print("The number is multipication by 7")
+else:
+    print("The number is not multipication by 7")
+
