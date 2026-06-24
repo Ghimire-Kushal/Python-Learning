94893f00f0a4635901e1b0bd38f799475e04cc5f
Kushal Ghimire <kushal.upr@gmail.com>
2026-06-24 13:29:05 +0545

    chapter 3 set

---
+++ Chapter 3/set.py
@@ -0,0 +1,15 @@
+student ={
+    "name": "Kushal",
+    "age": 20,
+    "grade": "A"
+
+}
+print(student["name"])
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(thisdict["model"])
