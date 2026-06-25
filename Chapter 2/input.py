eb0fb0da35644d010a4eb50ad5221b253e3a9ebc
Kushal Ghimire <kushal.upr@gmail.com>
2026-06-26 05:14:26 +0545

    loop

--- Chapter 2/input.py
+++ Chapter 2/input.py
@@ -1,10 +1,34 @@
-# sum of the given input number
-a = int(input("Enter the number 1: "))
-b = int(input("Enter the number 2: "))
+# # sum of the given input number
+# a = int(input("Enter the number 1: "))
+# b = int(input("Enter the number 2: "))
 
 
-print ("Number a: ",a)
-print ("Number b: ",b)
+# print ("Number a: ",a)
+# print ("Number b: ",b)
 
-print("sum is :", a+b)
+# print("sum is :", a+b)
  
+
+
+
+# multiplication of table 
+
+
+
+for i in range(1, 11):
+    print("2 x ", i, " = ", 2*i)
+
+
+
+
+# calculate the multiplication table 2 to 10 
+
+for j in range(2, 11):
+    for i in range(1, 11):
+        print(j, " x ", i, " = ", j*i)
+    print()
+
+
+
+
+
